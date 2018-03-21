95725ecc91be18769e280623e26a1692879a0286
Michael Hoffer <info@michaelhoffer.de>
2018-03-21 16:36:38 +0100

    code layout

--- src/occ-csg.cpp
+++ src/occ-csg.cpp
@@ -147,7 +147,7 @@ int main(int argc, char *argv[])
 	if(argc > 1 && strcmp(argv[1], "--version")==0) { version(); exit(0); }
 	
 	std::cout << "------------------------------------------------------------" << std::endl;
-        std::cout << "------        CSG based on the OCE CAD Kernel         ------" << std::endl;
+    std::cout << "------        CSG based on the OCE CAD Kernel         ------" << std::endl;
 	std::cout << "------                  Version " << VERSION << "                   ------" << std::endl;
 	std::cout << "------ 2018 by Michael Hoffer (info@michaelhoffer.de) ------" << std::endl;
 	std::cout << "------                www.mihosoft.eu                 ------" << std::endl;
