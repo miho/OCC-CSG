947604887dd4de53600d29419acf05c90cf11fd0
Michael Hoffer <info@michaelhoffer.de>
2021-08-25 17:44:41 +0200

    move to dynamic linking on windows

--- configure.ps1
+++ configure.ps1
@@ -3,6 +3,6 @@
 $env:VCPKG_ROOT="\dev\vcpkg\"
 
 # configure triplet
-$env:VCPKG_TRIPLET="x64-windows-static"
+$env:VCPKG_TRIPLET="x64-windows"
 
 cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE="${env:VCPKG_ROOT}\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET="${env:VCPKG_TRIPLET}"
